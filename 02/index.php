bf200a29c9772e4a47e3bb5b656371bd5e36f774
Dave Dick <david.dck@gmail.com>
2026-01-14 15:36:24 -0500

    First Pass in PHP

--- 02/index.php
+++ 02/index.php
@@ -1,16 +1,42 @@
+<?php
 //1. Set Up & Start 
-
+declare (strict_types=1);
 
 //2. Code Commenting 
 
+// <- inline
+
+/* 
+    Multi 
+*/
 
 //3. Variables, Data Tyoes, Concatenation, Conditional Statements & Echo
+$firstName = "Dave";
+$lastName = "Dick";
 
+//Concatenation Example:
+echo "<p> Hello there, my name is " . $firstName . " " . $lastName . "</p>";
 
 //4. Loosely Typed Language Demo
+// $num1 = 1;
+// $num2 = "10";
 
+// function AddLoosely($num1, $num2)
+// {
+//     return $num1 + $num2;
+// }
+
+// echo "<p>" . add($num1, $num2) . "</p>";
 
 //5. Strict Types & Types Hints
+$num1 = 1;
+$num2 = 10;
 
+function add(int $num1, int $num2) : int
+{
+    return $num1 + $num2;
+}
+
+echo "<p>" . add($num1, $num2) . "</p>";
 
 //6. OOP with PHP 
